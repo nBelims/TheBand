773a5f506b32fc6bb9aa7cfa8d6cfb963fe548a0
nBelims <anhnguyen150301@gmail.com>
2022-11-22 20:38:45 +0700

    update responsive

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const modal = document.querySelector('.js-modal');
 
 const buyBtnList = document.querySelectorAll('.js-buy-ticket');
 // console.log(buyBtnList);
-
+    // Modal logic
 const modalClose = document.querySelector('.modal-close');
 const modalContainer = document.querySelector('.modal-container');
 
@@ -23,3 +23,32 @@ modal.addEventListener('click',hideModal);
 modalContainer.addEventListener('click',function(e) {
     e.stopPropagation();
 });
+    // Navigation mobile logic
+const barBtn = document.querySelector('.bar-btn');
+const header = document.querySelector('#header');
+const headerHeight = header.clientHeight;
+
+const showNavigation = function() {
+
+} 
+barBtn.onclick = function() {
+    const isClosed = header.clientHeight === headerHeight;
+    if (isClosed) {
+        header.style.height = 'auto';
+    }   else {
+        header.style.height = null;
+    }
+    barBtn.classList.toggle('active', isClosed);
+}
+    // SubNav mobile logic 
+    const navList = document.querySelectorAll('#nav li');
+    console.log(navList);
+    navList.forEach(function(nav) {
+        nav.onclick = function(e) {
+            if (this.id == 'js-more') {
+                e.preventDefault();
+            } else {
+                header.style.height = null;
+            }
+        }
+    })
